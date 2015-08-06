0f86a2d189c355e26400552abaa75599b400c412
walter.michelin <walter.michelin@weebly.com>
2015-08-06 15:22:03 -0700

    fixed client-server scoring mode consistency, cleaned up styling for scoreboard

--- ui/public/components/leaderboard/LeaderboardComponent.js
+++ ui/public/components/leaderboard/LeaderboardComponent.js
@@ -16,48 +16,48 @@ var LeaderboardComponent = module.exports = React.createClass({
 
 
 
-    getInitialState: function() {
-        return {
-            players: [],
-            active: true
-        };
-    },
+        getInitialState: function() {
+            return {
+                players: [],
+                active: true
+            };
+        },
 
 
 
-    componentDidMount: function() {
-        
-        var _this = this;
-        
-        this.getLeaderboard();
-        
-        //node.socket.on('leaderboard.show', _this.show);
-        //node.socket.on('leaderboard.hide', _this.hide);
+        componentDidMount: function() {
 
-    },
-    
-    
-    
-    show: function() {
-        this.getLeaderboard();
-        this.setState({
-            active: true
-        });
-    },
-    
-    
-    
-    hide: function() {
-        this.setState({
-            active: false
-        });
-    },
-    
-    
-    
-    getLeaderboard: function() {
-        var _this = this;
-        $.get(config.clientUrl + '/leaderboard', function(players) {
+            var _this = this;
+
+            this.getLeaderboard();
+
+            //node.socket.on('leaderboard.show', _this.show);
+            //node.socket.on('leaderboard.hide', _this.hide);
+
+        },
+
+
+
+        show: function() {
+            this.getLeaderboard();
+            this.setState({
+                active: true
+            });
+        },
+
+
+
+        hide: function() {
+            this.setState({
+                active: false
+            });
+        },
+
+
+
+        getLeaderboard: function() {
+            var _this = this;
+            $.get(config.clientUrl + '/leaderboard', function(players) {
             _this.setState({
                 players: players
             });

cffba36e8bb30b66d8b5cdc4c15fb30010746f08
Tim Elfelt <timelf123@gmail.com>
2015-12-08 20:07:37 -0600

    starting seeds

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,13 @@ module.exports = {
   development: {
     client: config.development.database.client,
     connection: config.development.database.connection,
-    migrations: config.development.database.migrations
+    migrations: config.development.database.migrations,
+    seeds: config.development.database.seeds
+  },
+  production: {
+    client: config.production.database.client,
+    connection: config.production.database.connection,
+    migrations: config.production.database.migrations,
+    seeds: config.production.database.seeds
   }
 };
